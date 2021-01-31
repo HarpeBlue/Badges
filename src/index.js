e3ff8ad37914256e2682453a695704b22840f643
HarpeBlue <hspe2507@gmail.com>
2021-01-30 21:31:21 -0500

    add: react.createElement()

--- src/index.js
+++ src/index.js
@@ -8,9 +8,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = React.createElement(
+  'a',
+  { href: 'https://platzi.com' },
+  'Hola! Soy los children.'
+  )
+
+const nombre = 'Harold';
+
+const element1 = React.createElement(
+  'h1',
+  {},
+  `Hola! Soy ${nombre}.`
+)
+
+const element2 = React.createElement(
+  'div',
+  {},
+  React.createElement(
+    'h1',
+    {},
+    'Hola, soy Harold'
+  ),
+  React.createElement(
+    'p',
+    {},
+    'Soy desarrollador Frontend'
+  )
+)
+
+const jsx = <h1>Hello, yo soy {nombre}</h1>;
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(element2, container);
