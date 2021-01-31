62c1db268c6aa7af443b0749aaf6ed1a6ff5d81e
HarpeBlue <hspe2507@gmail.com>
2021-01-31 18:45:59 -0500

    add: bootstrap y estilos.

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,25 +2,27 @@ import React from 'react';
 
 import confLogo from "../images/badge-header.svg";
 
+import './styles/Badge.css';
+
 class Badge extends React.Component {
   render() {
     return (
-      <article>
-        <header>
+      <article className='Badge'>
+        <header className='Badge__header'>
           <img src={confLogo} alt="Logo de la conferencia"/>
         </header>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+        <div className='Badge__section-name'>
+          <img className='Badge__avatar' src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
           <h1>Harold Peñaloza</h1>
         </div>
 
-        <div>
-          <p>Frontend Developer</p>
-          <p>@HarpeBlue</p>
+        <div className='Badge__section-info'>
+          <h3>Frontend Developer</h3>
+          <div>@HarpeBlue</div>
         </div>
 
-        <footer>
+        <footer className='Badge__footer'>
           <p>#Platziconf</p>
         </footer>
 
